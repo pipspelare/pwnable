33e74e411a70f3fe5e3eb5c5ab303dcc381ad007
pipspelare <reqwsha@mail.ru>
2026-08-25 02:36:55 +1100

    collision!

--- collision.py
+++ collision.py
@@ -2,14 +2,14 @@ from pwn import p32, ssh, log
 
 code_hash = 0x21DD09EC
 num = code_hash // 5
-remaineder = code_hash % 5
+remainder = code_hash % 5
 
 buf = b""
 buf += p32(num)
 buf += p32(num)
 buf += p32(num)
 buf += p32(num)
-buf += p32(num+remaineder)
+buf += p32(num+remainder)
 
 r = ssh('col', 'pwnable.kr', password='guest', port=2222)
 p = r.process(executable='./col', argv=['col', buf])
